47d0bca0242fe0a103e0969c0d46c5c6cb2fdae2
Abhimanyu <abhimanyu4@gmail.com>
2023-12-08 10:09:54 -0800

    refactor the og file as well and match the functionality with the faster whisper file

--- whisper-og.py
+++ whisper-og.py
@@ -1,18 +1,23 @@
 # open-AIs version of the implmentation
 import os
 from os.path import isfile, join
+import argparse
 import psutil
 import ffmpeg
-import argparse
 import whisper
+import timeit
 from pathlib import Path
 from typing import Iterator, TextIO
 
 
 # Note:
-# by default large model is used
+# by default large model is used and float32 precision
 #
 #
+sizes_supported = ["tiny", "base", "small", "medium", "large-v1", "large-v2", "large-v3", "large"]
+
+video_supported = [".mkv", ".mov",  ".avi", ".mp4"]
+audio_supported = [".mp3", ".wave", ".aac", ".flac"]
 
 def srt_format_timestamp(seconds: float):
     assert seconds >= 0, "non-negative timestamp expected"
@@ -30,6 +35,10 @@ def srt_format_timestamp(seconds: float):
     return (f"{hours}:") + f"{minutes:02d}:{seconds:02d},{milliseconds:03d}"
 
 def write_srt(transcript: Iterator[dict], file: TextIO):
+
+    print("\nBegin transcription and creating subtitle file:")
+    print("-------------------------------------------------------")
+
     count = 0
     for segment in transcript:
         count +=1
@@ -43,38 +52,45 @@ def write_srt(transcript: Iterator[dict], file: TextIO):
 
 def isVideoFile(file_suffix):
     file_suffix = file_suffix.lower()
-    if file_suffix == ".mkv" or file_suffix == ".avi" or file_suffix == ".mp4":
+    if file_suffix in video_supported:
         return True
 
     return False
 
 def isAudioFile(file_suffix):
     file_suffix = file_suffix.lower()
-    if file_suffix == ".mp3" or file_suffix == ".wave" or file_suffix == ".aac":
+    if file_suffix in audio_supported:
         return True
 
     return False
 
 def transcribe(args, model, videofile, audiofile = None):
 
+    start_time = timeit.default_timer()
+
     if audiofile is None:
         audiofile = videofile
 
-    segments = model.transcribe(audiofile, verbose=True, language=args.language)
+    segments = model.transcribe(audiofile, verbose=True, beam_size=args.beam_size, language=args.language)
 
     # save SRT
-    print("\nBegin transcription and creating subtitles:")
-    print("-------------------------------------------------------")
-
     with open(join(args.output_dir, Path(videofile).stem + f".{args.language}.srt"), "w") as srt:
         write_srt(segments["segments"], file=srt)
 
+    print(videofile," took ","{:.1f}".format(timeit.default_timer() - start_time)," seconds")
+    print("-------------------------------------------------------")
+
 def initialize(args):
+
+    print("\nInitializing:")
+    print("-------------------------------------------------------")
+
     # cleanup the audio file that is no longer needed
     if isfile("output.mp3"):
         os.remove("output.mp3")
 
     os.environ["OMP_NUM_THREADS"] = str(args.nproc)
+
 
     # initialize the model with given args
     model = whisper.load_model(args.model_size, device=args.device)
@@ -82,8 +98,11 @@ def initialize(args):
     return model;
 
 def close():
+
     if isfile("output.mp3"):
         os.remove("output.mp3")
+
+    print("Done.")
 
 def main():
 
@@ -96,27 +115,18 @@ def main():
     parser.add_argument("--output_dir", "-o", help="Ouput directory", default=os.getcwd())
     parser.add_argument("--language", "-l", help="Language to be translated from", default='en', type=str)
     parser.add_argument("--beam_size", "-b", help="Beam size parameter or best_of equivalent from Open-AI whisper", type=int, default=5)
-    parser.add_argument("--device", "-d", help="Device to use such a CPU or GPU", default="cuda")
-    parser.add_argument("--model_size", "-s", help="Size of the model, default is large. For testing use tiny", default="large")
+    parser.add_argument("--device", "-d", help="Device to use such a CPU or GPU", choices=["cpu", "cuda"], default="cpu")
+    parser.add_argument("--model_size", "-s", help="Size of the model, default is large.", choices=sizes_supported, default="small")
     parser.add_argument("--nproc", "-n", help="Number of CPUs to use", default=psutil.cpu_count(logical=False), type=int)
 
     args = parser.parse_args()
-    print("\nSettings as follows:")
-    print("-------------------------------------------------------")
-
-    arguments = vars(args);
-    for arg in arguments:
-        print(arg, '\t', getattr(args, arg))
-
-    print("\nInitializing:")
-    print("-------------------------------------------------------")
-    model = initialize(args);
 
     if args.filename is not None:
-        if isVideoFile(Path(args.filename.name).suffix) == True:
-            video_files.append(args.filename.name)
-        elif isAudioFile(Path(args.filename.name).suffix) == True:
-            audio_files.append(args.filename.name)
+        args.filename = args.filename.name
+        if isVideoFile(Path(args.filename).suffix) == True:
+            video_files.append(args.filename)
+        elif isAudioFile(Path(args.filename).suffix) == True:
+            audio_files.append(args.filename)
 
     elif args.input_dir is not None:
         for f in os.listdir(args.input_dir):
@@ -128,6 +138,17 @@ def main():
     if len(video_files) == 0 and len(audio_files) == 0:
         print("There were no files to process")
         exit(0)
+    else:
+        print("Found ", len(video_files)+len(audio_files), " files")
+
+    print("\nSettings as follows:")
+    print("-------------------------------------------------------")
+
+    arguments = vars(args);
+    for arg in arguments:
+        print(arg, '\t', getattr(args, arg))
+
+    model = initialize(args);
 
     # convert the videofile into audiofile before processing
     for videoFile in video_files:
@@ -137,6 +158,7 @@ def main():
     # convert the audiofile before processing
     for audiofile in audio_files:
         transcribe(args, model, audiofile)
+
 
     # cleanup the audio file that is no longer needed
     close()
