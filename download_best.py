b669fe38c5ffe90a19060060574e69b621a3e13a
Abhimanyu <abhimanyu4@gmail.com>
2024-04-02 16:29:51 -0400

    Fine tune the download script
    Add more switches and make the switches clearer
    Fix the live output on the console for Windows
    Timeout minutes to wait for the download to finish is 15 min default

--- download_best.py
+++ download_best.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import sys
 import os
 import youtube_dl
 import argparse
@@ -6,12 +7,11 @@ import subprocess
 import shutil
 from pathlib import Path, PurePath
 
-
-global_model_bin        = "yt-dlp.exe"
-global_get_best_format  = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
-global_get_video_format = "bestvideo[ext=mp4]/best[ext=mp4]"
-global_get_audio_format = "bestaudio[ext=m4a]/best[ext=aac]"
-global_get_merge_format = 'mkv'
+default_model_bin        = "yt-dlp.exe"
+default_get_best_format  = "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
+default_get_video_format = "bestvideo[ext=mp4]/best[ext=mp4]"
+default_get_audio_format = "bestaudio[ext=m4a]/best[ext=aac]"
+default_get_merge_format = 'mkv'
 
 class Download:
 
@@ -58,14 +58,16 @@ class Download:
 		if args.keep:
 			self.opts += ["-k"]
 
-		if args.output:
-			self.opts += ["-o", str(Path(args.output_dir, args.output))]
+		if args.output_name:
+			self.opts += ["-o", str(Path(args.output_dir, args.output_name))]
 
 		if args.verbose:
 			self.opts += ["--verbose"]
 
-		if args.merge:
-			self.opts += ["--merge-output-format", args.merge_format]
+		if args.merge is not None and args.merge:
+			self.opts += ["--merge-output-format", args.merge[0]]
+		elif args.merge is not None:
+			self.opts += ["--merge-output-format", self.get_merge_format]
 
 		if args.overwrite:
 			self.opts += ["--yes-overwrites"]
@@ -75,34 +77,42 @@ class Download:
 			if args.password:
 				self.opts += ["-p", args.password]
 
-		if args.bin:
-			self.model_bin = str(Path(args.bin).resolve())
+		if args.timeout:
+			self.timeout = args.timeout
+
+		try:
+			if args.bin:
+				self.model_bin = str(Path(args.bin).resolve())
+		except FileNotFoundError as e:
+		 	print(e)
+
 
 	def get_youtube_vid(self):
 
-		process = subprocess.Popen([self.model_bin] + self.opts, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		process = subprocess.Popen([self.model_bin] + self.opts, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1)
 
 		if self.debug_flag == True:
 			print(f"Starting process {process}\n")
 
 		video_name = ""
-		stdout_line = "start";
 
-		while stdout_line:
+		while True:
 
 			stdout_line_bytes = process.stdout.readline()
 
-			# Check if stdout_line_bytes is empty, indicating the end of output
-			if not stdout_line_bytes:
-				stderr_line = process.stderr.readline().decode('utf-8')
+			if not stdout_line_bytes and process.poll() is not None:
+				break
+
+			elif not stdout_line_bytes:
+				stderr_line = process.stderr.readline()
 				if stderr_line:
-					print(stderr_line.strip())
+					print(stderr_line.decode('utf-8').rstrip())
 				break
 
 			try:
 				# Attempt to decode the line as UTF-8
 				stdout_line = stdout_line_bytes.decode('utf-8')
-				stdout_line = stdout_line.strip()
+				stdout_line = stdout_line.rstrip()
 			except UnicodeDecodeError as e:
 				# Handle decoding error gracefully
 				print(f"Error decoding line from stdout: {e}")
@@ -111,7 +121,7 @@ class Download:
 
 			# if just listing the formats, then stop here
 			if "-F" in self.opts:
-				print(stdout_line)
+				print(stdout_line, flush=True)
 				continue;
 			#out, err = process.communicate() #blocking process
 
@@ -122,23 +132,24 @@ class Download:
 
 			if stdout_line.startswith(merger_pattern):
 				video_name = stdout_line[len(merger_pattern):-1]
-				print(stdout_line)
+				print(stdout_line, flush=True)
 				continue
 
 			elif not video_name and stdout_line.startswith(download_pattern):
 				video_name = stdout_line[len(download_pattern):]
-				print(stdout_line)
+				print(stdout_line, flush=True)
 				continue
 
 			elif downloaded_pattern in stdout_line:
 				video_name = stdout_line[len("[download] "):stdout_line.index(downloaded_pattern)]
-				print(stdout_line)
+				print(stdout_line, flush=True)
 				break
 
-			print(stdout_line)
+			elif stdout_line:
+				print(stdout_line, flush=True)
 
 		# Wait for the process to finish
-		return_code = process.wait()
+		return_code = process.wait(timeout=self.timeout) # default 15 min
 
 		#if args.output_dir != os.getcwd():
 		#	shutil.move(os.getcwd(), args.output_dir)
@@ -161,19 +172,24 @@ class Download:
 
 	def __init__(self, args, debug = False):
 
-		global global_model_bin
-		global global_get_best_format
-		global global_get_video_format
-		global global_get_audio_format
-		global global_get_merge_format
+		global default_model_bin
+		global default_get_best_format
+		global default_get_video_format
+		global default_get_audio_format
+		global default_get_merge_format
 
-		self.debug_flag       = debug
-		self.model_bin        = global_model_bin
-		self.get_best_format  = global_get_best_format
-		self.get_audio_format = global_get_audio_format
-		self.get_video_format = global_get_video_format
-		self.get_merge_format = global_get_merge_format
-		self.opts             = []
+		# validate the executatable
+		if shutil.which(default_model_bin) is not None:
+			self.debug_flag       = debug
+			self.model_bin        = default_model_bin
+			self.get_best_format  = default_get_best_format
+			self.get_audio_format = default_get_audio_format
+			self.get_video_format = default_get_video_format
+			self.get_merge_format = default_get_merge_format
+			self.opts             = []
+			self.timeout          = 900
+		else:
+			raise FileNotFoundError(f"Executable file has a problem or does not exist {default_model_bin}")
 
 		# parse input arguments
 		self.adjust_format(args)
@@ -185,24 +201,24 @@ def main():
 	parser.add_argument("-F", "--list", help="List all formats that can be downloaded", action='store_true')
 	parser.add_argument("--verbose", help="Verbose output", action='store_true')
 	parser.add_argument('-k', "--keep", help="Keep intermediate files", action='store_true')
-	parser.add_argument("-f", "--format", help="Format of the video to download", default=global_get_best_format)
+	parser.add_argument("-f", "--format", help="Format of the video to download", default=default_get_best_format)
 	parser.add_argument("-u", "--username", help="Username to login with")
 	parser.add_argument("-p", "--password", help="Password for the credentials. Used with username")
-	parser.add_argument("-o", "--output", help="Ouput format")
+	parser.add_argument("-o", "--output_name", help="Ouput format")
 	parser.add_argument("-d", "--output_dir", help="Ouput directory", default=os.getcwd())
 	parser.add_argument("-a", "--audio_only", help="Audio only download", action='store_true')
 	parser.add_argument("-v", "--video_only", help="Video only download", action='store_true')
-	parser.add_argument("-b", "--bin", help="Path to the binary to choose", default=global_model_bin)
-	parser.add_argument("-m", "--merge", help="Bool on whether to merge the audio and video", type=bool, default=False)
-	parser.add_argument("--merge_format", help="Fomat of merging the audio and video. Default format: mkv", default=global_get_merge_format)
+	parser.add_argument("-b", "--bin", help="Path to the binary to choose")
+	parser.add_argument("-m", "--merge", help="Whether to merge the audio and video. Default format: mkv", nargs='*')
 	parser.add_argument("--quiet", help="Debug print off", action='store_true')
 	parser.add_argument("--overwrite", help="Overwrite an exising file", action='store_true')
+	parser.add_argument("--timeout", help="Amount of time to wait for the download to finish. Default 15 min")
 
 	args = parser.parse_args()
 	downloader = Download(args, debug=not args.quiet)
 
 	# make the directory if missing
-	Path(args.output_dir).mkdir(parents=True, exist_ok=True)
+	Path(args.output_dir).resolve().mkdir(parents=True, exist_ok=True)
 
 	if not args.quiet:
 
