66e67c8a377e59ac8de41d1f601901b8a589dbfd
Abhimanyu <abhimanyu4@gmail.com>
2024-04-04 13:45:51 -0400

    Moved all code in the audiofile into a class
    It can be called externally from now.
    Audio filters can be applied from now.
    Noise filtering or background noise rejection can be used
    
    AAC does not export a seek table so cannot tell how long the audio is
    without the -s and -e explicitly specifying.
    
    Need something like '-c:a copy' to fix.

--- getaudio.py
+++ getaudio.py
@@ -5,108 +5,143 @@ from pathlib import Path, PurePath
 import time
 import re
 
-video_supported = ["mkv", "webm", "mov",  "avi", "mp4"]
-audio_supported = ["mp3", "wave", "aac", "flac"]
+class AudioProcess:
 
-def isVideoFile(file_suffix):
-	if file_suffix.split('.')[-1].lower() in video_supported:
-		return True
+	@staticmethod
+	def valid_time(time_str):
+		pattern = r'^\d{2}:\d{2}:\d{2}$'  # Regular expression pattern for xx:xx:xx format
+		if not re.match(pattern, time_str):
+			raise argparse.ArgumentTypeError(f"Invalid time format: {time_str}. Must be in the format xx:xx:xx")
+		return time_str
 
-	return False
+	def extract_audio(self):
 
-def isAudioFile(file_suffix):
-	if file_suffix.lower() in audio_supported:
-		return True
+		time.sleep(1)
 
-	return False
+		print("\nRunning:")
+		print("-------------------------------------------------------")
 
-def valid_time(time_str):
-	pattern = r'^\d{2}:\d{2}:\d{2}$'  # Regular expression pattern for xx:xx:xx format
-	if not re.match(pattern, time_str):
-		raise argparse.ArgumentTypeError(f"Invalid time format: {time_str}. Must be in the format xx:xx:xx")
-	return time_str
+		for media_file in self.media_files:
+			output_filename = str(Path(self.opts.output_dir, media_file.stem + "." + time.strftime("%Y%m%d-%H%M%S") + "." + self.opts.extension))
+			print("\tOutput file", '\t', output_filename)
 
-def extract_audio(args):
+			if self.opts.start and self.opts.end:
+				stream = ffmpeg.input(str(media_file), ss=self.opts.start, to=self.opts.end)
+			elif self.opts.start:
+				stream = ffmpeg.input(str(media_file), ss=self.opts.start)
+			elif self.opts.end:
+				stream = ffmpeg.input(str(media_file), to=self.opts.end)
+			else:
+				stream = ffmpeg.input(str(media_file))
 
-	video_files = []
-	if args.filename is not None:
-		args.filename = Path(args.input_dir, args.filename.name)
-		if isVideoFile(args.filename.suffix) == True:
-			video_files.append(args.filename)
+			if self.opts.audio_filter:
+				stream = ffmpeg.output(stream, output_filename, af=self.opts.audio_filter)
+			else:
+				stream = ffmpeg.output(stream, output_filename)
 
-	elif args.input_dir is not None:
-		for filename in os.listdir(args.input_dir):
-			filename = Path(args.input_dir, filename)
-			if filename.is_file() and isVideoFile(filename.suffix) == True:
-				video_files.append(filename);
+			print(f"ffmpeg compiled as {stream.compile()}")
+			# Start the subprocess
+			try:
+				if self.opts.overwrite:
+					out, err = ffmpeg.run(stream, capture_stdout=True, capture_stderr=True, overwrite_output=True)
+				else:
+					out, err = ffmpeg.run(stream, capture_stdout=True, capture_stderr=True)
+			except ffmpeg.Error as e:
+			    print("FFmpeg error:", e.stderr.decode())
+			else:
+			    print("FFmpeg output:", out.decode())
+			    print("FFmpeg error:", err.decode())
 
-	if len(video_files) == 0:
-		print("There were no files to process")
-		exit(0)
-	else:
-		print("Found ", len(video_files), " files")
-		if args.output_dir is None:
-			args.output_dir = video_files[0].parents[0]
+		print("\nFinished:")
+		print("-------------------------------------------------------")
 
-	# make the directory if missing
-	Path(args.output_dir).mkdir(parents=True, exist_ok=True)
 
-	print("\nSettings as follows:")
-	print("-------------------------------------------------------")
-
-	arguments = vars(args);
-	for arg in arguments:
-		print(arg, '\t', getattr(args, arg))
-
-	# join all arguments for the program
-	if args.start and args.end:
-		options = " ".join(["-ss", args.start, "-to", args.end])
-	elif args.start:
-		options = " ".join(["-ss", args.start]);
-	elif args.end:
-		options = " ".join(["-to", args.end]);
-	else:
-		options = ""
-
-	time.sleep(1)
-	
-	print("\nRunning:")
-	print("-------------------------------------------------------")
-
-	for videofile in video_files:
-		output_filename = str(Path(args.output_dir, videofile.stem + "." + time.strftime("%Y%m%d-%H%M%S") + "." + args.format))
-		print("\tOutput file", '\t', output_filename)
-		
-		if args.start and args.end:
-			ffmpeg.input(str(videofile), ss=args.start, to=args.end).output(output_filename).run()
-		elif args.start:
-			ffmpeg.input(str(videofile), ss=args.start).output(output_filename).run()
-		elif args.end:
-			ffmpeg.input(str(videofile), to=args.end).output(output_filename).run()
+	def init_params(self):
+		if self.opts.input_dir is None:
+			self.opts.input_dir = os.getcwd()
 		else:
-			ffmpeg.input(str(videofile)).output(output_filename).run()
+			try:
+				self.opts.input_dir = str(Path(self.opts.input_dir).resolve())
+			except FileNotFoundError  as e:
+				print(f"self.opts.input_dir argument error: {e}")
 
-		print("\tDone")
+		if self.opts.model:
+			try:
+				self.opts.model = str(Path(self.opts.model).resolve())
+				self.opts.model = self.opts.model.replace('\\', '/')
+				self.asmr_filter = 'highpass=f=50,lowpass=f=18200,afftdn,dynaudnorm,loudnorm,arnndn=m={self.opts.model}'
 
-	print("\nFinished:")
-	print("-------------------------------------------------------")
+			except FileNotFoundError  as e:
+				print(f"self.opts.model argument error: {e}")
+
+		if self.opts.output_dir is not None:
+			try:
+				self.opts.output_dir = str(Path(self.opts.output_dir).resolve(strict=False))
+			except FileNotFoundError  as e:
+				print(f"self.opts.output_dir argument error: {e}")
+
+
+	def __init__(self, args, debug=False):
+		self.input_args = ""
+		self.output_args = ""
+		self.media_files = []
+		self.debug = debug
+		self.opts = args
+		self.asmr_filter = ''
+
+		# init and check for valid paths
+		self.init_params()
+
+		if self.opts.filename is not None:
+			self.opts.filename = Path(self.opts.input_dir, self.opts.filename.name)
+			self.media_files.append(self.opts.filename)
+
+		elif self.opts.input_dir is not None:
+			for filename in os.listdir(self.opts.input_dir):
+				filename = Path(self.opts.input_dir, filename)
+				if filename.is_file():
+					self.media_files.append(filename);
+
+		if len(self.media_files) == 0:
+			print("There were no files to process")
+			exit(0)
+
+		print("Found ", len(self.media_files), " files")
+		if self.opts.output_dir is None:
+			self.opts.output_dir = self.media_files[0].parents[0]
+
+		# make the directory if missing
+		Path(self.opts.output_dir).mkdir(parents=True, exist_ok=True)
+
 
 def main():
 	parser = argparse.ArgumentParser("Generates audio of the video file as an input")
-	parser.add_argument("-i", "--input_dir", help="Input directory where video files are", default=os.getcwd())
-	parser.add_argument("-f", "--filename", help="Name of the video file that needs to subtitles", type=argparse.FileType('r', encoding='UTF-8'))
+	parser.add_argument("-i", "--filename", help="Name of the media file that needs to subtitles", type=argparse.FileType('r', encoding='UTF-8'))
+	parser.add_argument("--input_dir", help="Input directory where video files are")
 	parser.add_argument("-o", "--output_dir", help="Ouput directory")
-	parser.add_argument("--format", help="Ouput format to produce", choices=audio_supported, default="mp3")
-	parser.add_argument("-s", "--start", help="Start time in 00:00:00 format", type=valid_time)
-	parser.add_argument("-e", "--end", help="End time in 00:00:00 format", type=valid_time)
+	parser.add_argument("-x", "--extension", help="Codec to use to produce the disired extension", default="mp3")
+	parser.add_argument("-af", "--audio_filter", help="Audio or video filters to use, no spaces, just comma-separated")
+	parser.add_argument("-m", "--model", help="Path to the RNN model to use for noise supression")
+	parser.add_argument("-s", "--start", help="Start time in 00:00:00 format", type=AudioProcess.valid_time)
+	parser.add_argument("-e", "--end", help="End time in 00:00:00 format", type=AudioProcess.valid_time)
+	parser.add_argument("-y", "--overwrite", help="Overwrite the file if exists", action='store_true')
+	parser.add_argument("--noenc", help="Simply copy the audio component (without encoder) into a new container of the exension given. \
+						Without this AAC for e.g. does not create a seek table", action='store_true')
+	parser.add_argument("--quiet", help="Debug print off", action='store_true')
+
 
 	args = parser.parse_args()
-	args.input_dir = str(Path(args.input_dir).resolve())
 
-	extract_audio(args)
+	if args.quiet == False:
+		print("\nSettings as follows:")
+		print("-------------------------------------------------------")
 
-	# cleanup the audio file that is no longer needed
-	# close()
+		arguments = vars(args)
+		for arg in arguments:
+			print(arg, '\t', getattr(args, arg))
+
+	ffmpeg_handler = AudioProcess(args, debug=not args.quiet)
+	ffmpeg_handler.extract_audio();
 
 	return 0
 
